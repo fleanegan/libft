fbd8164a8d4d1f83fc788fad10671a89664013a4
Frederik SCHLUTER <fschlute@e2r12p10.clusters.42paris.fr>
2022-01-26 09:41:49 +0100

    added count_entries_in_2d_char_array

--- src/libft.h
+++ src/libft.h
@@ -86,5 +86,6 @@ int				ft_lstget_index_of_element(t_list *lst, t_list *element);
 int				ft_lstcompare(t_list *a, t_list *b, \
 				int (*compare)(void *, void *));
 unsigned int	ft_abs(int in);
+size_t			count_entries_in_2d_char_array(char **arr2d);
 
 #endif
