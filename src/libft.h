a67a09090d81643318467faf28593932fe515f24
Frederik SCHLUTER <fschlute@e2r12p9.clusters.42paris.fr>
2022-03-30 13:44:13 +0200

    normed

--- src/libft.h
+++ src/libft.h
@@ -100,6 +100,6 @@ int				generic_split_line_parser(char *str, void *container, \
 void			**ft_lsttoarray(t_list *in, size_t content_size);
 void			ft_lstrmnode(\
 				t_list **lst, t_list *node_to_delete, void (*del)(void *));
-void			**to_array(t_list *pList, int(cpy)(void *, void **));
+void			**to_array(t_list *pList, int (cpy)(void *, void **));
 
 #endif
